7e13ac84cb313e55a66b3d364959c6d0befa75b9
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2020-01-30 14:04:59 +0100

    Add project files

---
+++ udacity-c2-restapi-user/src/config/config.ts
@@ -0,0 +1,20 @@
+export const config = {
+  "dev": {
+    "username": process.env.UDAGRAM_POSTGRESS_USERNAME,
+    "password": process.env.UDAGRAM_POSTGRESS_PASSWORD,
+    "database": process.env.UDAGRAM_POSTGRESS_DATABASE,
+    "host": process.env.UDAGRAM_POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.UDAGRAM_AWS_REGION,
+    "aws_profile": process.env.UDAGRAM_AWS_PROFILE,
+    "aws_media_bucket": process.env.UDAGRAM_AWS_MEDIA_BUCKET,
+    "jwt_secret": process.env.UDAGRAM_JWT_SECRET
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
